2304d8c2780e4ed43d9cce1b5dc4ba6162c619c1
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2011-12-21 16:46:45 -0600

    Combined UITableView into package
    Signed-off-by: Kevin Kinnebrew <kevin.kinnebrew@gmail.com>

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,1 +1,27 @@
+/**
+ * mobile.js | OrangeUI Framework 0.1 | 12.21.2011 
+ */
+ 
 document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+$(document).ready(function() {
+
+	$(".ios-ui-bar-button-item").bind("touchstart", function(e) {
+		
+		$(this).addClass("ios-bar-button-gradient-touch");
+	
+	});
+	
+	$(".ios-ui-bar-button-item").bind("touchcancel", function(e) {
+		
+		$(".ios-ui-bar-button-item").removeClass("ios-bar-button-gradient-touch");
+	
+	});
+	
+	$(".ios-ui-bar-button-item").bind("touchend", function(e) {
+		
+		$(".ios-ui-bar-button-item").removeClass("ios-bar-button-gradient-touch");
+	
+	});
+
+});

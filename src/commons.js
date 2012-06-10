54f173039f8076f3655ef0ae1c950ebe32e92f15
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2012-06-10 14:56:49 -0500

    Updated sandbox commons
    Signed-off-by: Kevin Kinnebrew <kevin.kinnebrew@gmail.com>

--- src/commons.js
+++ src/commons.js
@@ -787,7 +787,7 @@
 			},
 			
 			searchString: function (data) {
-				for (var i=0;i<data.length;i++)	{
+				for (var i = 0; i < data.length; i++)	{
 					var dataString = data[i].string;
 					var dataProp = data[i].prop;
 					this.versionSearchString = data[i].versionSearch || data[i].identity;
@@ -807,93 +807,24 @@
 			},
 			
 			dataBrowser: [
-				{
-					string: navigator.userAgent,
-					subString: "Chrome",
-					identity: "Chrome"
-				},
-				{ 
-					string: navigator.userAgent,
-					subString: "OmniWeb",
-					versionSearch: "OmniWeb/",
-					identity: "OmniWeb"
-				},
-				{
-					string: navigator.vendor,
-					subString: "Apple",
-					identity: "Safari",
-					versionSearch: "Version"
-				},
-				{
-					prop: window.opera,
-					identity: "Opera",
-					versionSearch: "Version"
-				},
-				{
-					string: navigator.vendor,
-					subString: "iCab",
-					identity: "iCab"
-				},
-				{
-					string: navigator.vendor,
-					subString: "KDE",
-					identity: "Konqueror"
-				},
-				{
-					string: navigator.userAgent,
-					subString: "Firefox",
-					identity: "Firefox"
-				},
-				{
-					string: navigator.vendor,
-					subString: "Camino",
-					identity: "Camino"
-				},
-				{		// for newer Netscapes (6+)
-					string: navigator.userAgent,
-					subString: "Netscape",
-					identity: "Netscape"
-				},
-				{
-					string: navigator.userAgent,
-					subString: "MSIE",
-					identity: "Explorer",
-					versionSearch: "MSIE"
-				},
-				{
-					string: navigator.userAgent,
-					subString: "Gecko",
-					identity: "Mozilla",
-					versionSearch: "rv"
-				},
-				{ 		// for older Netscapes (4-)
-					string: navigator.userAgent,
-					subString: "Mozilla",
-					identity: "Netscape",
-					versionSearch: "Mozilla"
-				}
+				{ string: navigator.userAgent, subString: "Chrome", identity: "Chrome" },
+				{ string: navigator.userAgent, subString: "OmniWeb", versionSearch: "OmniWeb/", identity: "OmniWeb" },
+				{ string: navigator.vendor, subString: "Apple", identity: "Safari", versionSearch: "Version" },
+				{ prop: window.opera, identity: "Opera", versionSearch: "Version" },
+				{ string: navigator.vendor, subString: "iCab", identity: "iCab" },
+				{ string: navigator.vendor, subString: "KDE", identity: "Konqueror" },
+				{ string: navigator.userAgent, subString: "Firefox", identity: "Firefox" },
+				{ string: navigator.vendor, subString: "Camino", identity: "Camino" },
+				{	string: navigator.userAgent, subString: "Netscape", identity: "Netscape" },
+				{ string: navigator.userAgent, subString: "MSIE", identity: "Explorer", versionSearch: "MSIE" },
+				{ string: navigator.userAgent, subString: "Gecko", identity: "Mozilla", versionSearch: "rv" },
+				{ string: navigator.userAgent, subString: "≈", identity: "Netscape", versionSearch: "Mozilla" }
 			],
 			dataOS : [
-				{
-					string: navigator.platform,
-					subString: "Win",
-					identity: "Windows"
-				},
-				{
-					string: navigator.platform,
-					subString: "Mac",
-					identity: "Mac"
-				},
-				{
-					string: navigator.userAgent,
-					subString: "iPhone",
-					identity: "iPhone/iPod"
-		    },
-				{
-					string: navigator.platform,
-					subString: "Linux",
-					identity: "Linux"
-				}
+				{ string: navigator.platform, subString: "Win", identity: "Windows" },
+				{ string: navigator.platform, subString: "Mac", identity: "Mac" },
+				{ string: navigator.userAgent, subString: "iPhone", identity: "iPhone/iPod" },
+				{ string: navigator.platform, subString: "Linux", identity: "Linux" }
 			]
 		
 		};

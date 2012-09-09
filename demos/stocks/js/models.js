23b8d67105d63c336af0b0e6fd8b53e6d33872c7
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2012-09-08 22:56:27 -0500

    Updating demos
    Signed-off-by: Kevin Kinnebrew <kevin.kinnebrew@gmail.com>

--- demos/stocks/js/models.js
+++ demos/stocks/js/models.js
@@ -31,7 +31,7 @@ Orange.add('tickertype-models', function(exports) {
     
     getFields: function() {
       return {
-        id:          { type: Field.KEY, required: true, numeric: true },
+        id:          { type: Field.KEY, numeric: true },
         username:    { type: Field.TEXT, required: true },
         firstName:   { type: Field.TEXT, required: true },
         lastName:    { type: Field.TEXT, required: true },
@@ -50,7 +50,7 @@ Orange.add('tickertype-models', function(exports) {
     
     getFields: function() {
       return {
-        id:          { type: Field.KEY, required: true, numeric: true },
+        id:          { type: Field.KEY, numeric: true },
         name:        { type: Field.TEXT, required: true },
         shortRate:   { type: Field.PERCENT, precision: 0 },
         longRate:    { type: Field.PERCENT, precision: 0 },
@@ -68,7 +68,7 @@ Orange.add('tickertype-models', function(exports) {
     
     getFields: function() {
       return {
-        id:              { type: Field.KEY, required: true, numeric: true },
+        id:              { type: Field.KEY, numeric: true },
         price:           { type: Field.MONEY, required: true, currency: 'USD' },
         symbol:          { type: Field.MODEL, required: true, model: 'symbol' },
         quantity:        { type: Field.NUMBER },
@@ -87,10 +87,10 @@ Orange.add('tickertype-models', function(exports) {
     getFields: function() {
       return {
         id:          { type: Field.KEY },
-        symbol:      { type: Field.MODEL, model: 'symbol' },
-        tradeDate:   { type: Field.DATE },
-        quantity:    { type: Field.NUMBER },
-        cost:        { type: Field.MONEY, currency: 'USD' }
+        symbol:      { type: Field.MODEL, required: true, model: 'symbol' },
+        tradeDate:   { type: Field.DATE, required: true },
+        quantity:    { type: Field.NUMBER, required: true },
+        cost:        { type: Field.MONEY, required: true, currency: 'USD' }
       };
     }
     
@@ -105,11 +105,11 @@ Orange.add('tickertype-models', function(exports) {
     getFields: function() {
       return {
         id:            { type: Field.KEY },
-        symbol:        { type: Field.TEXT },
-        name:          { type: Field.TEXT },
-        price:         { type: Field.MONEY, currency: 'USD' },
-        priceChange:   { type: Field.MONEY, currency: 'USD' },
-        percentChange: { type: Field.PERCENT, precision: 2 },
+        symbol:        { type: Field.TEXT, required: true },
+        name:          { type: Field.TEXT, required: true },
+        price:         { type: Field.MONEY, required: true, currency: 'USD' },
+        priceChange:   { type: Field.MONEY, required: true, currency: 'USD' },
+        percentChange: { type: Field.PERCENT, required: true, precision: 2 },
         marketCap:     { type: Field.NUMBER },
         peRatio:       { type: Field.NUMBER },
         lastTradeDate: { type: Field.DATE },

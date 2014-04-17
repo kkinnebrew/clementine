1c96eae486c14925f18653813037ea24b7173043
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2014-04-17 15:19:56 -0500

    Version 0.5.3

--- clementine.js
+++ clementine.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-12 ClementineJS
+ * Copyright (c) 2010-14 ClementineJS
 
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the

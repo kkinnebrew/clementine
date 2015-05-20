49af7f94a22dd991f1395ef2c20efd97a73862c9
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2015-05-20 02:50:23 -0500

    Update clementine.js

--- clementine.js
+++ clementine.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-14 ClementineJS
+ * Copyright (c) 2010-15 ClementineJS
 
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
